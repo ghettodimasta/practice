ef6ad62ad9f55a4bd2adcc7854acb1cc435c2f53
Богдан Скворчевский <54885261+justtomu@users.noreply.github.com>
2021-07-07 18:45:01 +0300

    added algorithm

---
+++ src/dijkstra/Algorithm.java
@@ -0,0 +1,90 @@
+package dijkstra;
+
+import java.io.*;
+import java.util.ArrayList;
+
+
+public class Algorithm {
+
+    private Graph graph = new Graph();
+    private ArrayList<Node> nodes = new ArrayList<Node>();
+
+    Algorithm() {
+        this.run_alg();
+    }
+
+    public void run_alg() {
+        read_graph_from_file();
+
+        graph = Dijkstra.calculateShortestPathFromSource(this.graph, nodes.get(0));
+        for (Node node : graph.getNodes()) {
+            System.out.print(node.getName() + ' ');
+            System.out.println(node.getDistance());
+        }
+
+    }
+
+    private void read_graph_from_file() {
+        String data = Read("/Users/justtomu/IdeaProjects/pr1/src/graph.txt").strip();
+        ArrayList<Character> letters = new ArrayList<Character>();
+        int counter = 0;
+        while (counter < data.length()) {
+            char letter = data.charAt(counter);
+            if (Character.isAlphabetic(letter) && !letters.contains(letter)) {
+                letters.add(data.charAt(counter));
+            }
+            counter += 1;
+        }
+
+        for (Character letter : letters) {
+            nodes.add(new Node(letter.toString()));
+        }
+
+        for (String line : data.split("\n")) {
+            String[] edge = line.split(" ");
+            String name1 = edge[0];
+            Node node1 = null;
+            for (Node el : nodes) {
+                if (el.getName().equals(name1)) {
+                    node1 = el;
+                }
+            }
+            if (node1 == null) {
+                return;
+            }
+            String name2 = edge[1];
+            Node node2 = null;
+
+            for (Node el : nodes) {
+                if (el.getName().equals(name2)) {
+                    node2 = el;
+                }
+            }
+
+            if (node2 == null) {
+                return;
+            }
+
+            node1.addDestination(node2, Integer.parseInt(edge[2]));
+
+        }
+
+        for (Node node : nodes) {
+            this.graph.addNode(node);
+        }
+
+    }
+
+    public String Read(String filename){
+        StringBuilder content = new StringBuilder();
+        try (FileReader reader = new FileReader(filename)){
+            int i;
+            while ((i=reader.read()) != -1)
+                content.append((char) i);
+            return content.toString();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return "";
+    }
+}
---
+++ src/dijkstra/RunAlgorithm.java
@@ -0,0 +1,7 @@
+package dijkstra;
+
+public class RunAlgorithm {
+    public static void main(String[] args) {
+        Algorithm alg = new Algorithm();
+    }
+}
